f5bbb22f6caafac28f5692c2f64ba90a1bbef2cf
Taaboy <taaboy3@gmail.com>
2016-12-03 12:24:54 +1000

    Adding Creature Spawndist Movements
    Still in progress some may need to be tweaked

---
+++ sql/custom_fixes/AsharaCreatureMovTables.sql
@@ -0,0 +1,264 @@
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 35096;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 35111;
+UPDATE creature 
+SET 
+    spawndist = 40,
+    MovementType = 1
+WHERE
+    id = 35095;
+UPDATE creature 
+SET 
+    spawndist = 10,
+    MovementType = 1
+WHERE
+    id = 35312;
+UPDATE creature 
+SET 
+    spawndist = 10,
+    MovementType = 1
+WHERE
+    id = 35245;
+UPDATE creature 
+SET 
+    spawndist = 10,
+    MovementType = 1
+WHERE
+    id = 36936;
+UPDATE creature 
+SET 
+    spawndist = 40,
+    MovementType = 1
+WHERE
+    id = 35466;
+UPDATE creature 
+SET 
+    spawndist = 40,
+    MovementType = 1
+WHERE
+    id = 14593;
+UPDATE creature 
+SET 
+    spawndist = 60,
+    MovementType = 1
+WHERE
+    id = 17467;
+UPDATE creature 
+SET 
+    spawndist = 10,
+    MovementType = 1
+WHERE
+    id = 6118;
+UPDATE creature 
+SET 
+    spawndist = 40,
+    MovementType = 1
+WHERE
+    id = 36304;
+UPDATE creature 
+SET 
+    spawndist = 40,
+    MovementType = 1
+WHERE
+    id = 36147;
+UPDATE creature 
+SET 
+    spawndist = 60,
+    MovementType = 1
+WHERE
+    id = 37002;
+UPDATE creature 
+SET 
+    spawndist = 40,
+    MovementType = 1
+WHERE
+    id = 35833;
+UPDATE creature 
+SET 
+    spawndist = 40,
+    MovementType = 1
+WHERE
+    id = 35832;
+UPDATE creature 
+SET 
+    spawndist = 40,
+    MovementType = 1
+WHERE
+    id = 35831;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 6650;
+UPDATE creature 
+SET 
+    spawndist = 40,
+    MovementType = 1
+WHERE
+    id = 36868;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 7885;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 7886;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 6196;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 6195;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 6190;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 6352;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 13896;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 8761;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 6375;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 6377;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 8764;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 36816;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 8660;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 36611;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 36012;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 36015;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 35759;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 6201;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 6200;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 36637;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 36639;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 36640;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 36637;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 6370;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 36592;
+UPDATE creature 
+SET 
+    spawndist = 20,
+    MovementType = 1
+WHERE
+    id = 36593;
